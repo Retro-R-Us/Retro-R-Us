2611c1936432522590c4daf2822af8cc7f6da27f
Remics89 <Remics89@gmail.com>
2023-01-06 04:16:53 -0800

    import bcrypt, export createUser

--- db/models/user.js
+++ db/models/user.js
@@ -1,10 +1,14 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  createUser
 };
+
+
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
