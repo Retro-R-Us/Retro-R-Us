a3265738310067dec186d40422ba8f90029456fb
Remics89 <Remics89@gmail.com>
2023-01-06 04:17:53 -0800

    register user function created. salts user password

--- db/models/user.js
+++ db/models/user.js
@@ -8,7 +8,30 @@ module.exports = {
   createUser
 };
 
+async function createUser(userData) { 
+  const SALT = 10;
+  const { username, password, email } = userData;
 
+  try {
+    const hashedPassword = await bcrypt.hash(password, SALT);
+    const { rows: [user] } = await client.query(
+      `
+        INSERT INTO users (username, password, email)
+        VALUES ($1, $2, $3)
+        ON CONFLICT (username, email) DO NOTHING
+      `, [username, hashedPassword, email]
+    );
+
+    return {
+      Success: true,
+      Message: "User registration successful!"
+    }
+    
+  } catch (error) {
+    console.log("Could not create user.");
+    throw error;
+  }
+}
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
