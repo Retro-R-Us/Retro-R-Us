65ea203c91b43293dc638582278882b6b06d1e62
kaypaint <kelanmpainter@gmail.com>
2023-01-07 09:46:14 -0600

    feat(DB Games): Built DB function getGameById

--- db/models/games.js
+++ db/models/games.js
@@ -2,7 +2,8 @@ const client = require('../client');
 
 module.exports = {
     // add your database adapter fns here
-    getAllGames
+    getAllGames,
+    getGameById
 };
 
 // get all games
@@ -12,4 +13,25 @@ async function getAllGames() {
     `);
 
     return games;
+}
+
+// get game by ID
+async function getGameById(gameId) {
+    try {
+        const { rows: [game] } = await client.query(`
+            SELECT * FROM games
+            WHERE "gameId"=$1;
+        `, [gameId]);
+
+        if (!game) {
+            throw {
+                name: 'ErrorGameNotFound',
+                message: 'Could not find a game by that gameId'
+            };
+        }
+
+        return game;
+    } catch (error) {
+        throw error;
+    }
 }
