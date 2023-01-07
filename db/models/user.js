8773930a539c54264dd670b94b74599f637672ec
Remics89 <Remics89@gmail.com>
2023-01-07 08:00:01 -0800

    feat: update user password

--- db/models/user.js
+++ db/models/user.js
@@ -1,17 +1,18 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
 const bcrypt = require('bcrypt');
+const SALT = 10;
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
   createUser,
   getUserByUsername,
-  getUserById
+  getUserById,
+  newPassword
 };
 
 async function createUser(userData) { 
-  const SALT = 10;
   const { username, password, email } = userData;
 
   try {
@@ -90,3 +91,43 @@ async function getUserById(id) {
     throw error;
   }
 }
+
+async function newPassword ({username, oldPassword, newPassword}) {
+  const hashedPassword = await bcrypt.hash(newPassword, SALT);
+  try {
+    const { rows: [ user ] } = await client.query(
+      `
+        SELECT (password)
+        FROM users
+        WHERE username=$1;
+      `, [username]
+    );
+    const isValid = await bcrypt.compare(user.password, oldPassword);
+    if (!isValid) {
+      const invalid = new Error('Old password is incorrect.')
+      return {
+        Success: false,
+        Message: invalid
+      }
+    } else {
+      await client.query(
+        `
+          UPDATE users
+          SET password=$1
+          WHERE username=$2;
+        `, [hashedPassword, username]
+      );
+
+      return {
+        Success: true,
+        Message: 'User password updated successfully.'
+      }
+    }
+
+  } catch (error) {
+    console.log("Could not change the user's password.");
+    throw error
+  }
+}
+
+
