60df1c6a19d07f28a14c6496ade87d23acc3f40b
kaypaint <kelanmpainter@gmail.com>
2023-01-07 10:21:26 -0600

    feat(DB Accessories): Built DB function getAccessoryById

--- db/models/accessories.js
+++ db/models/accessories.js
@@ -3,6 +3,7 @@ const client = require('../client');
 module.exports = {
     // add your database adapter fns here
     getAllAccessories,
+    getAccessoryById
 };
 
 // get all accessories
@@ -14,8 +15,24 @@ async function getAllAccessories() {
     return accessories;
 }
 
-
 // get accessory by id
+async function getAccessoryById(accessoryId) {
+    try {
+        const { rows: [accessory] } = await client.query(`
+            SELECT * FROM accessories
+            WHERE "accessoryId"=$1;
+        `, [accessoryId]);
+
+        if (!accessory) {
+            throw {
+                name: 'ErrorAccessoryNotFound',
+                message: 'Could not find an accessory by that accessoryId'
+            };
+        }
+    } catch (error) {
+        throw error;
+    }
+}
 
 // get accessories by console
 
