3cb5cb4414da4f0d1e40b46e0114d6ce6a81d215
Remics89 <Remics89@gmail.com>
2023-01-09 00:34:11 -0800

    feat: user router

--- api/index.js
+++ api/index.js
@@ -13,5 +13,7 @@ apiRouter.get('/health', (req, res, next) => {
 });
 
 // place your routers here
+const userRouter = require('./user')
+apiRouter.use('/user', userRouter)
 
 module.exports = apiRouter;
