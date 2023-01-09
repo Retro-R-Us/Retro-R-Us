eaf580e44ea229b4251699e557b72311c078e4bf
kaypaint <kelanmpainter@gmail.com>
2023-01-08 20:59:31 -0600

    chore(Games API): Removed a couple commas from the ends of SQL queries

--- db/init_db.js
+++ db/init_db.js
@@ -53,7 +53,7 @@ async function buildTables() {
         CREATE TABLE orders (
           "orderId" SERIAL PRIMARY KEY,
           "userId" INTEGER REFERENCES users(id),
-          status varchar(255) NOT NULL,
+          status varchar(255) NOT NULL
         );
     `);
 
@@ -76,7 +76,7 @@ async function buildTables() {
           title varchar(50) NOT NULL,
           description varchar(50) NOT NULL,
           year integer NOT NULL,
-          price numeric(18,2) NOT NULL,
+          price numeric(18,2) NOT NULL
           );
         `);
 
