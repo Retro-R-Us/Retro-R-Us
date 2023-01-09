37b4f2074c946b6a67eeef9824b4ebdc2b43bc70
Remics89 <Remics89@gmail.com>
2023-01-09 05:50:32 -0800

    feat: register user route

--- api/user.js
+++ api/user.js
@@ -61,7 +61,22 @@ userRouter.post('/login', async (req, res, next) => {
 })
 
 userRouter.post('/register', async (req, res, next) => {
-    
+    try {
+        const { username, password, email } = req.body;
+        if (!(username && password && email)) {
+            res.status(400);
+            res.send({
+                Success: false,
+                Message: "A required field was missing. Please input required fields."
+            });
+        } else {
+            const response = await User.createUser(req.body);
+            res.status(200)
+            res.send(response)
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 userRouter.patch('/update', (req, res, next) => {
