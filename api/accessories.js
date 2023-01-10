ff1b1ecdf905417603e165946c5beb96597f6542
kaypaint <kelanmpainter@gmail.com>
2023-01-09 20:41:42 -0600

    feat(Accessories API): Set up accessories.js file

---
+++ api/accessories.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const accessoriesRouter = express.Router();
+const { Acc } = require('../db/models/index');
+
+// get all accessories
+
+// get accessory by id
+
+// get accessories by console
+
+// create an accessory listing
+
+// update an accessory listing
+
+// delete an accessory listing
+
+module.exports = accessoriesRouter;
