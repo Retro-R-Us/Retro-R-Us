5eb084442f4452eb7d75dcf8c31fc99ab85577df
kaypaint <kelanmpainter@gmail.com>
2023-01-09 18:34:43 -0600

    feat(Games API): Added route for specific game Id

--- api/games.js
+++ api/games.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const gamesRouter = express.Router();
-
-const {
-    Games
-} = require('../db/models/index');
+const { Games } = require('../db/models/index');
 
 gamesRouter.get('/', async (req, res, next) => {
     try {
         const allGames = await Games.getAllGames();
         res.send(allGames);
+    } catch (error) {
+        next(error);
+    }
+});
+
+gamesRouter.get('/:gameId', async (req, res, next) => {
+    try {
+        const game = await Games.getGameById(req.params.gameId);
+        res.send(game);
     } catch (error) {
         next(error);
     }
