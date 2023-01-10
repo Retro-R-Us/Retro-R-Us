2ea7e26c7d9cb252d304d9ea46ff426d911538c1
MShiers7 <martyshiers7@gmail.com>
2023-01-09 20:12:42 -0600

    feat: created get request for consoles

--- api/consoles.js
+++ api/consoles.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const router = express.Router();
-const {
-    Consoles
-} = require('../db/models/index')
+const consolesRouter = express.Router();
+const { Consoles } = require('../db/models/index');
+const jwt = require('jsonwebtoken');
+const { PK } = require('./PK');
 
-router.get('/', async(request, response, next) => {
+
+consolesRouter.get('/', async(request, response, next) => {
     try {
-        const allConsoles = await getAllConsoles()
+        const allConsoles = await Consoles.getAllConsoles()
         response.send(allConsoles)
+        console.log("An error occured while getting all consoles!")
     } catch (error) {
         next(error)
     }
-})
+});
+
+consolesRouter.post('/', async (request, response, next) => {
+    try {
+        const newConsole = await Consoles.createConsoleListing(request.body);
+        response.send(newConsole);
+    } catch (error) {
+        console.log("An error occured while creating a new console listing");
+        next(error);
+    }
+});
