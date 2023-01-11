a7a90b271c452a8e85eb787500c716feeed2a17a
MShiers7 <martyshiers7@gmail.com>
2023-01-10 20:19:55 -0600

    feat: made a get request for collectibles

---
+++ api/collectibles.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const collectiblesRouter = express.Router();
+const { Collectibles } = require('../db/models/index');
+const jwt = require('jsonwebtoken');
+const { PK } = require('./PK');
+
+collectiblesRouter.get('/', async(request, response, next) => {
+    try {
+        const allCollectibles = await Collectibles.getAllCollectibles()
+        response.send(allCollectibles)
+        console.log("An error occured while getting all collectibles!")
+    } catch (error) {
+        next(error)
+    }
+});
