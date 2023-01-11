7c3b15ec3d5ec92d3d2d9032ac9683555dd75322
jhoolwerf <89540718+jhoolwerf@users.noreply.github.com>
2023-01-10 19:12:46 -0600

    Update cart.js

--- api/cart.js
+++ api/cart.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cartRouter = express.Router;
-const { cart } = require('../db/models/index');
+const { Cart } = require('../db/models/index');
 
 cartRouter.post("/", async (req, res, next) => {
     try {
         const { orderId, quantity } = req.body;
-        const newOrder = await cart.addItemToCart ({ orderId, quantity });
+        const newOrder = await Cart.addItemToCart ({ orderId, quantity });
         res.send(newOrder);
     } catch (error) {
         console.error("Could not add item to cart")
@@ -16,9 +16,9 @@ cartRouter.post("/", async (req, res, next) => {
 cartRouter.delete("/:cartId", async (req, res, next) => {
     try {
         const { cartId } = req.params;
-        const cart = await getOrderById(id);
+        const cart = await Cart.getOrderById(id);
         if (req.cart === cart.cartId) {
-            const deletedCart = await cart.destroyCart(cartId);
+            const deletedCart = await Cart.destroyCart(cartId);
             res.send(deletedCart);
         }
 
@@ -31,7 +31,7 @@ cartRouter.delete("/:cartId", async (req, res, next) => {
 cartRouter.patch("/:cartId", async (req, res, next) => {
     try {
         const { cartId } = req.params;
-        const cart = await getOrderById(id);
+        const cart = await Cart.getOrderById(id);
         if (req.cart === cart.cartId) {
             const updatedCart = await updateCart(cartId);
             res.send(updatedCart);
