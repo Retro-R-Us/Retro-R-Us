8f7d58f7443ffd447b961300d105e44d58d7778d
kaypaint <kelanmpainter@gmail.com>
2023-01-10 20:34:30 -0600

    chore(cleanup): Rearranged apiRouter routes in api/index, added comments/console logs to api/games

--- api/index.js
+++ api/index.js
@@ -12,7 +12,6 @@ apiRouter.get('/health', (req, res, next) => {
   });
 });
 
-// place your routers here
 const userRouter = require('./user')
 apiRouter.use('/user', userRouter)
 
@@ -21,6 +20,12 @@ apiRouter.use('/accessories', accessoriesRouter)
 
 const consolesRouter = require('./consoles')
 apiRoputer.use('/consoles', consolesRouter)
+
+const cartRouter = require('./order');
+apiRouter.use("/order", cartRouter);
+
+const gamesRouter = require('./games'); // import the games router
+apiRouter.use('/games', gamesRouter); // mount the games router on /api/games
 
 apiRouter.get('*', (req, res, next) => {
   const err = new Error()
@@ -38,14 +43,5 @@ apiRouter.use((err, req, res, next) => {
     return next();
   }
 })
-
-// GAMES ROUTER
-const gamesRouter = require('./games'); // import the games router
-apiRouter.use('/games', gamesRouter); // mount the games router on /api/games
-
-
-//ROUTER: /api/order
-const cartRouter = require('./order');
-apiRouter.use("/order", cartRouter);
 
 module.exports = apiRouter;
