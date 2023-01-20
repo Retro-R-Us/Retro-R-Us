d56064c2e57a45d3c0803ac44b0db854ec985b9c
Remics89 <Remics89@gmail.com>
2023-01-19 17:30:56 -0800

    feat: API call: user reg/login, get user data

---
+++ src/api/users.js
@@ -0,0 +1,42 @@
+import BASEURL from ".";
+
+export const userAction = async (username, password, action) => {
+    try {
+        const response = await fetch(`${BASEURL}/users/${action}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("There was an error registering the user", error);
+    }
+};
+
+export const getCurrentUser = async (username, token) => {
+    try {
+        const response = await fetch(`${BASEURL}/users/me`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                username: username,
+            }),
+        });
+
+        const data = await response.json();
+        return data;
+    } catch {
+        console.error("Could not retrieve user", error);
+    }
+};
+
+
