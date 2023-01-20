2ccc9c6c1967c3531c146b02a78b36afe1b2a84a
Remics89 <Remics89@gmail.com>
2023-01-20 06:22:14 -0800

    feat: fully functional login/register component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,7 +5,11 @@ import { userAction } from '../api';
 const AuthorizeUser = (props) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
+    const [handle, setHandle] = useState("");
+    const [select, setSelect] = useState("");
+    const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(null);
+    const [style, setStyle] = useState();
     const { setUserData } = props;
 
     const { setToken } = props;
@@ -19,8 +23,11 @@ const AuthorizeUser = (props) => {
         formTitle = "Sign Up";
     }
 
+    const domains = [null, ".com", ".net", ".org"]
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+        const email = `${handle}${select}`;
         const data = {
             username: username,
             password: password,
@@ -31,12 +38,23 @@ const AuthorizeUser = (props) => {
         
         try {
             if (action === "login") {
-                const userData = await userAction(data);
-                setUserData(userData)
-                console.log("USERDATA:", userData)
+                const response = await userAction(data);
+                setUserData(response.userdata)
+                setStyle({display: "none"});
+                setSuccess(true);
+                setToken(response.userdata.token)
+                setMessage(response.Message)
+                setInterval(() => {
+                    history("/")
+                }, 5000)
             } else if (action === "register") {
                 const response = await userAction(data);
-                console.log("RESPONSE:", response)
+                setStyle({display: "none"});
+                setSuccess(true);
+                setMessage(response.Message);
+            } else { 
+                setMessage()
+                setSuccess(false)
             }
         } catch (error) {
             console.error(error)
@@ -49,24 +67,42 @@ const AuthorizeUser = (props) => {
             <form className="ui form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label>Username</label>
-                    <input type="text" name="Username" placeholder="Username" minLength="8" onChange={e => setUsername(e.target.value)} />
+                    <input type="text" name="Username" placeholder="Username" required minLength="8" onChange={e => setUsername(e.target.value)} />
                 </div>
                 <div className="field">
                     <label>Password</label>
-                    <input type="password" name="Password" placeholder="Password" minLength="8" onChange={e => setPassword(e.target.value)} />
+                    <input type="password" name="Password" placeholder="Password" required minLength="8" onChange={e => setPassword(e.target.value)} />
                 </div>
-                {action === "register" ? <div className="field">
+                {action === "register" ? 
+                <div className="field">
                     <label>Email</label>
-                        <input type="email" name="EmailHandle" placeholder="Email"  onChange={e => setEmail(e.target.value)} />
+                    <div className="ui input">
+                        <input type="Email" name="Email" placeholder="Email" required onChange={e => setHandle(e.target.value)} />
+                        <select value={select} required onChange={e => setSelect(e.target.value)} >
+                            {domains.map((domain, key) => (<option key={key} value={domain}>{domain}</option>))}
+                        </select>
+                    </div>
                 </div> : null}
-                <button className="ui button basic positive" type="submit" >Submit</button>
+                <button className="ui button basic positive" type="submit" style={style} >Submit</button>
             </form>
+            {success ? 
+                <div className="ui success message">
+                    <div>Success!</div>
+                    <p>{message}</p>
+                    {action === 'login' ? <p>You are now being redirected back to the homepage</p> : null}
+                </div> 
+            : 
+                <div className="ui success message">
+                    <div>Success!</div>
+                    <p>{message}</p>
+                    {action === 'login' ? <p>You are now being redirected back to the homepage</p> : null}
+                </div>
+            }
         </div>
     ) : null;
 }
 
 export default AuthorizeUser;
-
 
 {/* <div class="ui success message">
 <div class="header">Form Completed</div>
