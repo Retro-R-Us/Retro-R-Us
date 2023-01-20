7467034969cf4bef0b0bcb70a4a342ca67b5f639
Remics89 <Remics89@gmail.com>
2023-01-19 17:14:04 -0800

    refactor: starting front end API framework

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const BASEURL = "https://retro-r-us.onrender.com/api";
+
+export {} from "./accessories";
+export {} from "./collectibles";
+export {} from "./games"
+export {} from "./consoles"
+export {} from "./orders"
+export {} from "./cart"
+export {} from "./users"
+export {} from "./admin"
+
+export default BASEURL;
