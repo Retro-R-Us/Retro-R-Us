9717394a3dce19510822ad22b04595f07f5e7f48
kaypaint <kelanmpainter@gmail.com>
2023-01-21 09:59:25 -0600

    feat(Games Component): Created games component file, set up barebones component

---
+++ src/components/games.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Games = () => {
+
+    return (
+        <div className='gamesPage'>
+            <div className='gamesContainer'>
+                <h1>Games</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Games;
