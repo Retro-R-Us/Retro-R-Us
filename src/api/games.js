5e1d3e5d2f897d7e343def735cdd0de4e336b4d5
kaypaint <kelanmpainter@gmail.com>
2023-01-21 09:38:28 -0600

    feat(Games API): Added fetchGameById call

--- src/api/games.js
+++ src/api/games.js
@@ -3,7 +3,6 @@ import BASEURL from './index';
 export const fetchAllGames = async () => {
     try {
         const response = await fetch(`${BASEURL}/games`, {
-            method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
                 }
@@ -12,5 +11,19 @@ export const fetchAllGames = async () => {
         return data;
     } catch (error) {
         console.error("An error occured while attempting to fetch games:", error);
+    }
+}
+
+export const fetchGameById = async (gameId) => {
+    try {
+        const response = await fetch(`${BASEURL}/games/${gameId}`, {
+            headers: {
+                'Content-Type': 'application/json'
+                }
+            });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("An error occured while attempting to fetch game by ID:", error);
     }
 }
