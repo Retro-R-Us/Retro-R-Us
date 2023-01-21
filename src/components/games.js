a8d9ea76b9c0ec095f40eea37903a488a25fbb54
kaypaint <kelanmpainter@gmail.com>
2023-01-21 11:12:35 -0600

    feat(Games API): Set up Games component to display all games in cards

--- src/components/games.js
+++ src/components/games.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Games = () => {
+const Games = ({ games }) => {
 
     return (
         <div className='gamesPage'>
             <div className='gamesContainer'>
-                <h1>Games</h1>
+                {games && games.map(game => {
+                    return (
+                        <div className='gameCard' key={game.gameId}>
+                            <h1>{game.title}</h1>
+                            <h2>{game.description}</h2>
+                            <h3>{game.console}</h3>
+                            <h3>{game.year}</h3>
+                            <h2>{game.price}</h2>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
