2e64f3f23788cf772a9b00a473e88e4a690ac774
kaypaint <kelanmpainter@gmail.com>
2023-01-21 20:25:08 -0600

    feat(Accessories Component): Component recieves accessories, and maps cards for each

--- src/components/accessories.js
+++ src/components/accessories.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Accessories = () => {
+const Accessories = ({ accessories }) => {
     return (
-        <div>
-            <h1>Accessories</h1>
+        <div className="accesoriesPage">
+            <div className="accesoriesContainer">
+                {accessories && accessories.map(accessory => {
+                    return (
+                        <div className="accessoryCard" key={accessory.accessoryId}>
+                            <h1>{accessory.title}</h1>
+                            <h2>{accessory.description}</h2>
+                            <h3>{accessory.console}</h3>
+                            <h2>{accessory.price}</h2>
+                        </div>
+                    );
+                }
+                )}
+            </div>
         </div>
     );
 }
