6f8a13d70e3522fcceabfe8ab295c5b24bbb6fca
kaypaint <kelanmpainter@gmail.com>
2023-01-21 20:02:17 -0600

    feat(Accessories Component): Created Accessories component file & barebones setup

---
+++ src/api/accessories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Accessories = () => {
+
+    return (
+        <div className='accessoriesPage'>
+            <div className='accessoriesContainer'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Accessories;
