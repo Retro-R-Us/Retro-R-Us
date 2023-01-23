f07f81cf420ed6312da7a7b83a515bd07694a7bf
kaypaint <kelanmpainter@gmail.com>
2023-01-22 19:49:42 -0600

    feat(Search Component): Created Search.js, and set up file

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+    
+    return (
+        <div>
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+        </div>
+    );
+}
