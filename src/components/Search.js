c63062f6710c5ce00c8cbde221dedb93ae98a7a6
kaypaint <kelanmpainter@gmail.com>
2023-01-22 20:52:06 -0600

    feat(Search Component): Fixed/finished handleSearch, Search is passed down data depending on its parent, handleSearch filters data and sets parent state, and added onChange handler to the input.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-const Search = ({ data }) => {
+const Search = ({ data , setFilteredData }) => {
     const [search, setSearch] = React.useState("");
+
 
     const handleSearch = () => {
         // This filters the data, converts both the data and search to lowercase, and then checks if the data includes the search
         const filteredData = data.filter((item) => {
             return item.title.toLowerCase().includes(search.toLowerCase());
-        }
+        });
+        setFilteredData(filteredData);
     }
 
     return (
         <div>
-            <input type="text" placeholder="Search..." />
+            <input 
+                type="text" 
+                placeholder="Search..." 
+                value={search}
+                onChange={(event) => {
+                    setSearch(event.target.value);
+                }} 
+            />
             <button onClick={handleSearch}>Search</button>
         </div>
     );
