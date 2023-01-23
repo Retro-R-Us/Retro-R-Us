3844f1d61c8372a1c2db68a0542608cbe7a9b7bd
kaypaint <kelanmpainter@gmail.com>
2023-01-22 20:19:12 -0600

    feat(Search Component): Set up handleSearch, now passing data down from parent component. This allows the Search component to be used in multiple locations

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-const Search = () => {
+const Search = ({ data }) => {
     const [search, setSearch] = React.useState("");
+
+    const handleSearch = () => {
+        // This filters the data, converts both the data and search to lowercase, and then checks if the data includes the search
+        const filteredData = data.filter((item) => {
+            return item.title.toLowerCase().includes(search.toLowerCase());
+        }
+    }
 
     return (
         <div>
             <input type="text" placeholder="Search..." />
-            <button>Search</button>
+            <button onClick={handleSearch}>Search</button>
         </div>
     );
 }
