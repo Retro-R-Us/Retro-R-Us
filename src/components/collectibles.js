777ca325d05e39bf734d416a8af98d4d190061e7
MShiers7 <martyshiers7@gmail.com>
2023-01-22 20:30:33 -0600

    feat: rendered the collectibles component on the website

--- src/components/collectibles.js
+++ src/components/collectibles.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Collectibles = () => {
+const Collectibles = ({ collectibles }) => {
 
     return (
-        <div>
-            <h1>Collectibles</h1>
+        <div className='collectiblesPage'>
+        <div className='collectiblesContainer'>
+            {collectibles && collectibles.map(collectible => {
+                return (
+                    <div className='consoleCard' key={collectible.collectibleId}>
+                        <h1>{collectible.title}</h1>
+                        <h2>{collectible.description}</h2>
+                        <h3>{collectible.console}</h3>
+                        <h2>{collectible.price}</h2>
+                    </div>
+                );
+            })}
         </div>
+    </div>
     )
 }
 
