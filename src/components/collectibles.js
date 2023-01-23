3f86f2283602a7b78507f24819007e49ebf602b9
MShiers7 <martyshiers7@gmail.com>
2023-01-22 19:55:53 -0600

    feat: created basic collectibles component

---
+++ src/components/collectibles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Collectibles = () => {
+
+    return (
+        <div>
+            <h1>Collectibles</h1>
+        </div>
+    )
+}
+
+export default Collectibles;
