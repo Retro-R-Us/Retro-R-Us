223cff7ab66f50557bdf58eefc287a889d915c38
kaypaint <kelanmpainter@gmail.com>
2023-01-23 21:17:42 -0600

    feat(Search.js): Added search debouncing, and corrected prev commit, the h2 updated on every keystroke, this has been corrected by adding new state

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,29 @@ import React from "react";
 const Search = ({ data , setFilteredData }) => {
     const [search, setSearch] = React.useState("");
 
+    // This is the last search state, which is used to render the last search in the h2 'Showing results for...'
+    const [lastSearch, setLastSearch] = React.useState("");
+
+    // This is the timeout state, which is used to delay the search
+    const [timeout, setTimeout] = React.useState(null);
+
+    // When the user types in the input, handleSearchInput is called by onChange
+    const handleSearchInput = (event) => {
+        // Sets the search state to the value of the input
+        setSearch(event.target.value);
+
+        // If there is a timeout, clear it
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        // Set a new timeout, which calls handleSearch after 500ms
+        // This is debouncing, which means the search is only called after the user has finished typing for 500ms
+        setTimeout(setTimeout(() => {
+            handleSearch();
+            setLastSearch(search);
+        }, 500));
+    }
 
     const handleSearch = () => {
         // This filters the data, converts both the data and search to lowercase, and then checks if the data includes the search
@@ -18,12 +41,10 @@ const Search = ({ data , setFilteredData }) => {
                 type="text" 
                 placeholder="Search..." 
                 value={search}
-                onChange={(event) => {
-                    setSearch(event.target.value);
-                }} 
+                onChange={handleSearchInput}
             />
             <button onClick={handleSearch}>Search</button>
-            {search && <h2>Showing results for '{search}'</h2>}
+            {lastSearch && <h2>Showing results for '{lastSearch}'</h2>}
         </div>
     );
 }
