f203f2ba2a737d4363b56da040a865e9ed0b8500
MShiers7 <martyshiers7@gmail.com>
2023-01-24 20:46:17 -0600

    feat: completed the search update for consoles

--- src/components/consoles.js
+++ src/components/consoles.js
@@ -3,22 +3,34 @@ import Search from './Search';
 
 const Consoles = ({ consoles }) => {
     const [filteredData, setFilteredData] = React.useState ([]);
+
+
     return (
         <div className='consolesPage'>
         <div className='consolesContainer'>
-            {consoles && consoles.map(console => {
-                return (
-                    <div className='consoleCard' key={console.consoleId}>
-                        <h1>{console.title}</h1>
-                        <h2>{console.description}</h2>
-                        <h3>{console.year}</h3>
-                        <h2>{console.price}</h2>
-                    </div>
-                );
-            })}
+        <Search data={consoles} setFilteredData={setFilteredData} />
+                {filteredData.length > 0 ? (
+                    filteredData.map((console, index) => (
+                        <div className='consoleCard' key={index}>
+                            <h1>{console.title}</h1>
+                            <h2>{console.description}</h2>
+                            <h4>{console.year}</h4>
+                            <h2>{console.price}</h2>
+                        </div>
+                    ))
+                ) : (
+                    consoles.map((console, index) => (
+                        <div className='consoleCard' key={index}>
+                            <h1>{console.title}</h1>
+                            <h2>{console.description}</h2>
+                            <h4>{console.year}</h4>
+                            <h2>{console.price}</h2>
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
-    </div>
-    )
-}
+    );
+}  
 
 export default Consoles;
