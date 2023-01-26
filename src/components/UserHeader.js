ca49f8e39c1d6e1a7bb945d754d5801bd924151f
Remics89 <Remics89@gmail.com>
2023-01-26 00:20:11 -0800

    feat: separate menu nav for user actions

---
+++ src/components/UserHeader.js
@@ -0,0 +1,68 @@
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const UserHeader = (props) => {
+    const { token, username, setUsername, logOut, setModalTrigger, setAction } = props;
+
+    useEffect(() => {
+        setUsername(window.localStorage.getItem("username"));
+    }, [username]);
+
+    return (
+        <Fragment>
+            {token ? null : (
+                <div className="item">
+                    <button
+                        className="ui inverted primary button"
+                        onClick={() => {
+                            setAction("register"), setModalTrigger(true);
+                        }}>
+                        Sign Up
+                    </button>
+                </div>
+            )}
+            {token ? (
+                <div className="item">
+                    <button className="ui button" onClick={logOut}>
+                        Log Out
+                    </button>
+                </div>
+            ) : (
+                <div className="item">
+                    <button
+                        className="ui inverted primary button"
+                        onClick={() => {
+                            setAction("login"), setModalTrigger(true);
+                        }}>
+                        Login
+                    </button>
+                </div>
+            )}
+            <div className="item">
+                <Link className="ui animated button" to="/user/cart" tabIndex="0">
+                    <div className="hidden content">Cart</div>
+                    <div className="visible content">
+                        <i className="shop icon"></i>
+                    </div>
+                </Link>
+            </div>
+            {token ? (
+                <Fragment>
+                    <div className="item authbar">
+                        <Link className="ui button" to="/account" style={{color: "green"}}>
+                            <i aria-hidden="true" className="green users icon"></i>
+                            {username}
+                        </Link>
+                    </div>
+                </Fragment>
+            ) : (
+                <div className="item authbar" style={{color: "red"}}>
+                    <i aria-hidden="true" className="red users icon"></i>
+                    Not Logged In
+                </div>
+            )}
+        </Fragment>
+    );
+};
+
+export default UserHeader;
