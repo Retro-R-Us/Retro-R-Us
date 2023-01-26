2e31df46bbb17a2bda388729e1e72fc9076ad201
Remics89 <Remics89@gmail.com>
2023-01-26 01:01:37 -0800

    refact: export proper component name

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 
 export { default as App } from './App';
-export { default as Orders } from './orders';
+export { default as myOrders } from './orders';
 export { default as Consoles } from "./consoles";
 export { default as Cart } from './Cart';
