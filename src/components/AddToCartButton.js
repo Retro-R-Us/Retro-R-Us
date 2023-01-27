6a6f70eb68937484003614dd301ca1bf818cebeb
kaypaint <kelanmpainter@gmail.com>
2023-01-26 19:58:06 -0600

    feat(AddToCartButton.js): Created component file, basic setup

---
+++ src/components/AddToCartButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddToCartButton = () => {
+    return (
+        <button className="addToCartButton">Add to Cart</button>
+    );
+}
+
+export default AddToCartButton;
