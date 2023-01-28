6e2025e011ed4afae607e0a29aa6ee5d0044c452
Remics89 <Remics89@gmail.com>
2023-01-28 08:17:40 -0800

    added table framework for order data, close onClick state to false

--- src/components/orders.js
+++ src/components/orders.js
@@ -2,7 +2,7 @@ import BASEURL from "./index";
 import React, { useEffect } from "react";
 
 const MyOrders = ({ setOrdersTrigger, userData }) => {
-
+    
 
 
     return (
@@ -12,7 +12,7 @@ const MyOrders = ({ setOrdersTrigger, userData }) => {
                     id="close-btn"
                     className="ui animated fade button"
                     tabIndex="0"
-                    onClick={() => exitFunc()}>
+                    onClick={() => setOrdersTrigger(false)}>
                     <div className="visible content">
                         <i className="close icon"></i>
                     </div>
@@ -23,6 +23,24 @@ const MyOrders = ({ setOrdersTrigger, userData }) => {
                         <div className="ui header">Your Orders</div>
                     </div>
                 </div>
+                {<table className="ui celled table">
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Account Created:</th>
+                            <th>Phone #</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-label="username"></td>
+                            <td data-label="email"></td>
+                            <td data-label="accountAge"></td>
+                            <td data-label="phone">800-867-5309</td>
+                        </tr>
+                    </tbody>
+                </table>}
             </div>
         </div>
   )
