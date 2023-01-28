17e2d29ff6f2deea2356da1387c703579b2605b9
kaypaint <kelanmpainter@gmail.com>
2023-01-28 10:18:31 -0600

    feat(Admin Tools): Added state for admin action, formData, and set up rudimentary form, missing some features

--- src/components/admin.js
+++ src/components/admin.js
@@ -3,6 +3,17 @@ import React from 'react';
 // Admin.js is passed down userData from App.js
 // This is to be used to check if "isAdmin" = true
 const Admin = ({ userData }) => {
+    // State to keep track of which action the admin wants to perform
+    const [action, setAction] = React.useState(null);
+
+    // State to keep track of form data
+    const [formData, setFormData] = React.useState({
+        title: '',
+        description: '',
+        console: '',
+        year: '',
+        price: ''
+    });
 
     // If userData is not defined (no user logged in), return null for this component
     if (!userData) return null;
@@ -10,8 +21,20 @@ const Admin = ({ userData }) => {
     // If userData is defined, check if "isAdmin" = true, if so, return the Admin component
     if (userData.isAdmin) {
         return (
-            <div>
-                <h1>Admin Page</h1>
+            <div className='adminContainer'>
+                <div className='adminCard'>
+                    <form>
+                        <label>
+                            <h2>Select an option:</h2>
+                            <select>
+                                <option value='POST'>Add New</option>
+                                <option value='UPDATE'>Edit Existing</option>
+                                <option value='DELETE'>Delete Existing</option>
+                            </select>
+                        </label>
+                        <input type='submit' value='Submit' />
+                    </form>
+                </div>
             </div>
         )
     }
