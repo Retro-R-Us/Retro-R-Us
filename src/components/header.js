03db74799af8440443f3ef033db5d47ea05c9a58
Remics89 <Remics89@gmail.com>
2023-01-28 13:04:53 -0800

    feat: added category options menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,34 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const categories = ["Consoles", "Games", "Collectibles", "Accessories"];
+    const nav = useNavigate();
+
     return (
         <Fragment>
             <div className="item">
                 <Link className="ui button" to="/">
                     Home
                 </Link>
+            </div>
+            <div className="item">
+                <select className="ui button" onChange={(e) => {
+                    console.log(e.target.value)
+                    if (e.target.value === "") {
+                        return;
+                    }
+                    nav(e.target.value)
+                }}>
+                    <option value="">Browse</option>
+                    {categories.map((cat, index) => {
+                        return (
+                            <option value={cat} key={index}>
+                                {cat}
+                            </option>
+                        )
+                    })}
+                </select>
             </div>
             <header className="siteTitle">Retro-R-Us</header>
         </Fragment>
