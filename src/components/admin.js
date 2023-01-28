0d172465f5d5881f520b7306646b88baa8451308
kaypaint <kelanmpainter@gmail.com>
2023-01-28 09:52:34 -0600

    feat(Admin Tools): Added userData prop, added logic checking if user is an admin

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Admin = () => {
-    return (
-        <div>
-            <h1>Admin</h1>
-        </div>
-    );
+// Admin.js is passed down userData from App.js
+// This is to be used to check if "isAdmin" = true
+const Admin = ({ userData }) => {
+
+    // If userData is not defined (no user logged in), return null for this component
+    if (!userData) return null;
+
+    // If userData is defined, check if "isAdmin" = true, if so, return the Admin component
+    if (userData.isAdmin) {
+        return (
+            <div>
+                <h1>Admin Page</h1>
+            </div>
+        )
+    }
 }
 
 export default Admin;
