31c7921381faca6d53d91b6bbba2ebfc19b1e7f6
Jasmine Hoolwerf <jasmine.hoolwerf@gmail.com>
2023-01-28 19:44:26 -0600

    feat: Styling main page

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 const Home = () => {
     return (
-        <div className='ui cards'>
-        <div className="card">
+        <>
+            <div className="card">
             <h1>Weekly Tournaments</h1>
             <img src="https://cdn.evbuc.com/images/416863809/555909054187/1/logo.20230103-021945"></img>
             <p>Play Mortal Kombat in our upcoming tournament!</p>
@@ -13,7 +13,7 @@ const Home = () => {
             <img src="https://www.roadsideamerica.com/attract/images/tx/TXFRIvideo_arcade2_620x300.jpg"></img>
             <p>If you're in Frisco, TX take a step back in time and visit the National Video Game Museum. The only museum where you get to touch the things!</p>
         </div>
-        </div>
+        </>
 )
 }
 
