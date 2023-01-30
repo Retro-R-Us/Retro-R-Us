545a2577ff3b18e7497a6467641eb67a871c4716
kaypaint <kelanmpainter@gmail.com>
2023-01-29 22:23:27 -0600

    feat(App Routing): Added /admin route, passed in required data

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, createPath } from "react-router-dom";
 import { getAPIHealth, getCurrentUser } from "../api";
 import AuthorizeUser from "./Auth";
 import { Account } from "./account";
@@ -16,6 +16,7 @@ import Accessories from "./accessories";
 import { fetchAllAccessories } from "../api/accessories";
 import Home from "./home";
 import Cart from "./Cart";
+import Admin from "./admin";
 
 const localStorageCart = JSON.parse(localStorage.getItem("cart") || "[]")
 
@@ -143,6 +144,7 @@ const App = () => {
                 />
                 <Route path="/account" element={<Account userData={userData} token={token} userOrders={userOrders} />} />
                 <Route path="/accessories" element={<Accessories accessories={accessories} userData={userData}/>} />
+                <Route path="/admin" element={<Admin games={games} consoles={consoles} collectibles={collectibles} accessories={accessories}/>} />
             </Routes>
 
             {/* <Footer /> */}
