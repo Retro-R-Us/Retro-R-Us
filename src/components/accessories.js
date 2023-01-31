d53d87d4096396ddcd1d874de7ac068d58a539cd
Remics89 <Remics89@gmail.com>
2023-01-31 05:23:58 -0800

    feat: new styles to cards, added manual styling, imported test image & cards.css

--- src/components/accessories.js
+++ src/components/accessories.js
@@ -1,38 +1,72 @@
 import React from "react";
 import Search from "./Search";
 import AddToCartButton from "./AddToCartButton";
+import "../style/cards.css";
+import n64con from "./Product_Images/n64con.png";
 
-const Accessories = ({setCartItem, accessories, userData }) => {
+const Accessories = ({ setCartItem, accessories, userData }) => {
     const [filteredData, setFilteredData] = React.useState([]);
 
     return (
         <div className="accesoriesPage">
             <Search data={accessories} setFilteredData={setFilteredData} />
-            <div className="accesoriesContainer">
-                {filteredData.length > 0 ? (
-                    filteredData.map((accessory, index) => (
-                        <div className="accessoryCard" key={index}>
-                            <h2>{accessory.title}</h2>
-                            <h3>{accessory.description}</h3>
-                            <h3>{accessory.console}</h3>
-                            <h4>{accessory.price}</h4>
-                            <AddToCartButton data={accessory} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                ) : (
-                    accessories.map((accessory, index) => (
-                        <div className="accessoryCard" key={index}>
-                            <h2>{accessory.title}</h2>
-                            <h3>{accessory.description}</h3>
-                            <h3>{accessory.console}</h3>
-                            <h4>{accessory.price}</h4>
-                            <AddToCartButton data={accessory} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                )}
+            <div className="accessoriesContainer">
+                <div className="ui link cards">
+                    {filteredData.length > 0
+                        ? filteredData.map((accessory, index) => (
+                              <div className="card accCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image">
+                                      <img src="" />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{accessory.title}</div>
+                                      <div className="meta">
+                                          <a>Console: {accessory.console}</a>
+                                      </div>
+                                      <div className="description">
+                                        {accessory.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="right floated">${accessory.price}</span>
+                                  </div>
+                                  <h4>{accessory.price}</h4>
+                                  <AddToCartButton
+                                      data={accessory}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))
+                        : accessories.map((accessory, index) => (
+                              <div className="card accCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image" style={{marginRight: "30px"}} >
+                                      <img src={n64con} />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{accessory.title}</div>
+                                      <div className="meta">
+                                          <a>Console: {accessory.console}</a>
+                                      </div>
+                                      <div className="description">
+                                        {accessory.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="floated">${accessory.price}</span>
+                                  </div>
+                                  <h4></h4>
+                                  <AddToCartButton
+                                      data={accessory}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))}
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default Accessories;
