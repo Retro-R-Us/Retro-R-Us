74029ef06d995e04a8f366a29e17e18da1ee9c01
Remics89 <Remics89@gmail.com>
2023-01-31 04:09:37 -0800

    refactor: moved search into upper div

--- src/components/accessories.js
+++ src/components/accessories.js
@@ -7,8 +7,8 @@ const Accessories = ({setCartItem, accessories, userData }) => {
 
     return (
         <div className="accesoriesPage">
-            <div className="accesoriesContainer">
             <Search data={accessories} setFilteredData={setFilteredData} />
+            <div className="accesoriesContainer">
                 {filteredData.length > 0 ? (
                     filteredData.map((accessory, index) => (
                         <div className="accessoryCard" key={index}>
