56c230323b1f126b6e1e8e49f1aa0cbe4e7a2f51
Remics89 <Remics89@gmail.com>
2023-01-31 00:26:33 -0800

    refactor: green checkout btn, empty cart msg, changed remove method, total method

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -9,13 +8,11 @@ const Cart = ({ cart, setCart }) => {
         return;
         // setCart([]);
     };
-
-    // const shoppingCart = ((a, c) => a + c.price * c.quantity, 0);
-
+    
     const total = () => {
         let total = 0;
-        cart.map((item) => {
-            item.price = Number(item.price)
+        cart.every((item) => {
+            item.price = Number(item.price);
             const price = item.price;
             total += price;
         });
@@ -24,9 +21,9 @@ const Cart = ({ cart, setCart }) => {
 
     const removeItem = (item) => {
         const windowData = JSON.parse(window.localStorage.getItem("cart"));
-        windowData.every((x, index) => {
+        windowData.forEach((x, index) => {
             if (x.title === item.title) {
-                const removedItem = windowData.splice(index, index + 1);
+                const removedItem = windowData.splice(index, 1);
                 window.localStorage.setItem("cart", JSON.stringify(windowData));
                 setCart(windowData);
                 return;
@@ -102,12 +99,10 @@ const Cart = ({ cart, setCart }) => {
                     </tbody>
                 </table>
             ) : (
-              <div className="ui warning message">
-                <div className="header">
-                  Your Cart Is Empty.
+                <div className="ui warning message">
+                    <div className="header">Your Cart Is Empty.</div>
+                    <p>Nothing to See Here.</p>
                 </div>
-                <p>Nothing to See Here.</p>
-              </div>
             )}
             {cart.length > 0 ? (
                 <>
@@ -116,9 +111,9 @@ const Cart = ({ cart, setCart }) => {
                         style={{ marginRight: "15px" }}>
                         <p>Subtotal (before tax): ${total()}</p>
                     </div>
-                    <Link className="ui animated fade button" to="/checkout">
+                    <Link className="ui green animated fade button" to="/checkout">
                         <div className="visible content">Checkout</div>
-                        <div className="hidden content">
+                        <div id="checkoutbutton" className="hidden content">
                             <i className="shopping cart icon"></i>
                             <i className="exchange alternate icon"></i>
                             <i className="dollar sign icon"></i>
@@ -131,20 +126,3 @@ const Cart = ({ cart, setCart }) => {
 };
 
 export default Cart;
-
-{
-    /* <tr>
-    <td>
-        <h2 className="ui center aligned header">A</h2>
-    </td>
-    <td className="single line">Power Output</td>
-    <td>
-        <div className="ui star rating" data-rating="3" data-max-rating="3"></div>
-    </td>
-    <td className="right aligned">
-        80%
-        <br />
-        <a href="#">18 studies</a>
-    </td>
-</tr>; */
-}
