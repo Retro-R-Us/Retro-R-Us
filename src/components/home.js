03b4c0623007e156659abf8419a7b07fa5e03e0d
Remics89 <Remics89@gmail.com>
2023-01-31 04:05:25 -0800

    refactor: renamed classname card to homecard

--- src/components/home.js
+++ src/components/home.js
@@ -13,20 +13,19 @@ const Home = () => {
                 <li><span>Image5</span></li>
                 <li><span>Image6</span></li>
             </ul>
-        <div className="homeContainer">
-            
-        <img src={logo} className="logo" alt="logo"></img>
-            <div className="card">
-                <h1>Weekly Tournaments</h1>
-                <img src="https://cdn.evbuc.com/images/416863809/555909054187/1/logo.20230103-021945"></img>
-                <p>Play Mortal Kombat in our upcoming tournament!</p>
+            <div className="homeContainer">
+                <img src={logo} className="logo" alt="logo"></img>
+                <div className="homecard">
+                    <h1>Weekly Tournaments</h1>
+                    <img src="https://cdn.evbuc.com/images/416863809/555909054187/1/logo.20230103-021945"></img>
+                    <p>Play Mortal Kombat in our upcoming tournament!</p>
+                </div>
+                <div className="homecard">
+                    <h1>Visit the National Video Game Museum</h1>
+                    <img src="https://www.roadsideamerica.com/attract/images/tx/TXFRIvideo_arcade2_620x300.jpg"></img>
+                    <p>If you're in Frisco, TX take a step back in time and visit the National Video Game Museum. The only museum where you get to touch the things!</p>
+                </div>
             </div>
-            <div className="card">
-                <h1>Visit the National Video Game Museum</h1>
-                <img src="https://www.roadsideamerica.com/attract/images/tx/TXFRIvideo_arcade2_620x300.jpg"></img>
-                <p>If you're in Frisco, TX take a step back in time and visit the National Video Game Museum. The only museum where you get to touch the things!</p>
-            </div>
-        </div>
         </>
 )
 }
