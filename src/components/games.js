68e2c7f11132d6497ff5bfdc088883e4feb5cff4
Remics89 <Remics89@gmail.com>
2023-01-31 05:53:48 -0800

    feat: new styles to cards, added manual styling, imported test image & cards.css

--- src/components/games.js
+++ src/components/games.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Search from './Search';
 import AddToCartButton from './AddToCartButton';
+import DK from "./Product_Images/DonkeyKong.png"
+import "../style/cards.css"
 
 const Games = ({setCartItem, games, userData }) => {
     const [filteredData, setFilteredData] = React.useState([]);
@@ -15,35 +17,73 @@ const Games = ({setCartItem, games, userData }) => {
     */
 
     return (    
-        <div className='gamesPage'>
+        <div className="gamesPage">
             <Search data={games} setFilteredData={setFilteredData} />
-            <div className='gamesContainer'>
-                {filteredData.length > 0 ? (
-                    filteredData.map((game, index) => (
-                        <div className='gameCard' key={index}>
-                            <h2>{game.title}</h2>
-                            <h3>{game.description}</h3>
-                            <h3>{game.console}</h3>
-                            <h4>{game.year}</h4>
-                            <h4>${game.price}</h4>
-                            <AddToCartButton data={game} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                ) : (
-                    games.map((game, index) => (
-                        <div className='gameCard' key={index}>
-                            <h2>{game.title}</h2>
-                            <h3>{game.description}</h3>
-                            <h3>{game.console}</h3>
-                            <h4>{game.year}</h4>
-                            <h4>${game.price}</h4>
-                            <AddToCartButton data={game} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                )}
+            <div className="gamesContainer">
+                <div className="ui link cards">
+                    {filteredData.length > 0
+                        ? filteredData.map((game, index) => (
+                              <div className="card gameCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image">
+                                      <img src="" />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{game.title}</div>
+                                      <div className="meta">
+                                          <a>Console: {game.console}</a>
+                                      </div>
+                                      <div className="meta">
+                                          <a>Year Released: {game.year}</a>
+                                      </div>
+                                      <div className="description">
+                                        {game.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="right floated">${game.price}</span>
+                                  </div>
+                                  <h4>{game.price}</h4>
+                                  <AddToCartButton
+                                      data={game}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))
+                        : games.map((game, index) => (
+                              <div className="card gameCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image" style={{marginRight: "30px"}} >
+                                      <img src={DK} />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{game.title}</div>
+                                      <div className="meta">
+                                          <a>Console: {game.console}</a>
+                                      </div>
+                                      <div className="meta">
+                                          <a>Year Released: {game.year}</a>
+                                      </div>
+                                      <div className="description">
+                                        {game.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="floated">${game.price}</span>
+                                  </div>
+                                  <h4></h4>
+                                  <AddToCartButton
+                                      data={game}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))}
+                </div>
             </div>
         </div>
     );
 }
 
 export default Games;
+
+
