0b10c1d3e5ca805f55060b51eeab04a3adffeb83
Remics89 <Remics89@gmail.com>
2023-01-31 05:32:15 -0800

    feat: new styles to cards, added manual styling, imported cards.css

--- src/components/consoles.js
+++ src/components/consoles.js
@@ -1,38 +1,69 @@
 import React from 'react';
 import Search from './Search';
 import AddToCartButton from './AddToCartButton';
+import "../style/cards.css"
 
 
 const Consoles = ({ setCartItem, userData, consoles }) => {
     const [filteredData, setFilteredData] = React.useState ([]);
- 
 
     return (
-        <div className='consolesPage'>
+        <div className="consolesPage">
             <Search data={consoles} setFilteredData={setFilteredData} />
-            <div className='consolesContainer'>
-                {filteredData.length > 0 ? (
-                    filteredData.map((console, index) => (
-                        <div className='consoleCard' key={index}>
-                            <h2>{console.title}</h2>
-                            <img alt={console.image} src={`/components/Product_Images/${console.image}`}/>
-                            <h3>{console.description}</h3>
-                            <h4>{console.year}</h4>
-                            <h4>${console.price}</h4>
-                            <AddToCartButton data={console} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                ) : (
-                    consoles.map((console, index) => (
-                        <div className='consoleCard' key={index}>
-                            <h2>{console.title}</h2>
-                            <h3>{console.description}</h3>
-                            <h4>{console.year}</h4>
-                            <h4>${console.price}</h4>
-                            <AddToCartButton data={console} userData={userData} setCartItem={setCartItem} />
-                        </div>
-                    ))
-                )}
+            <div className="consolesContainer">
+                <div className="ui link cards">
+                    {filteredData.length > 0
+                        ? filteredData.map((console, index) => (
+                              <div className="card conCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image">
+                                      <img src="" />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{console.title}</div>
+                                      <div className="meta">
+                                          <a>Year Released: {console.year}</a>
+                                      </div>
+                                      <div className="description">
+                                        {console.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="right floated">${console.price}</span>
+                                  </div>
+                                  <h4>{console.price}</h4>
+                                  <AddToCartButton
+                                      data={console}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))
+                        : consoles.map((console, index) => (
+                              <div className="card conCard" key={index} style={{background: "none", backgroundColor: "rgba(138,192,240,.8)"}}>
+                                  <div className="image" style={{marginRight: "30px"}} >
+                                      <img src="" />
+                                  </div>
+                                  <div className="content">
+                                      <div className="header">{console.title}</div>
+                                      <div className="meta">
+                                          <a>Year Released: {console.year}</a>
+                                      </div>
+                                      <div className="description">
+                                        {console.description}
+                                      </div>
+                                  </div>
+                                  <div className="extra content">
+                                      <span className="floated">${console.price}</span>
+                                  </div>
+                                  <h4></h4>
+                                  <AddToCartButton
+                                      data={console}
+                                      userData={userData}
+                                      setCartItem={setCartItem}
+                                  />
+                              </div>
+                          ))}
+                </div>
             </div>
         </div>
     );
